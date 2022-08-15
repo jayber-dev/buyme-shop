d093a7927534fda006c3e3d7ccf4c4ffe7a7cf93
jay ber <jayber1@gmail.com>
2022-08-15 11:55:55 +0300

    init

---
+++ db/db.php
@@ -0,0 +1,15 @@
+<?php
+// DB querys related file
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
