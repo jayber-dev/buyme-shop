62bb9439dbd7d1e16eb4a5361f73c6ff558dc718
jayber <jayber1@gmail.com>
2022-08-15 17:03:44 +0300

    extends footer and navbar fix

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./static/js/index.js" defer></script>
+    <link rel="stylesheet" href="/static/css/index.css">
     <title>Document</title>
 </head>
 <body>
     
+    <?php
+        include './serve/navbar.php';
+    ?>
+    <a href="lala.php"> lala</a>
+    div
+
+    <?php
+        include './serve/footer.php'
+    ?>
+       
+    <!-- //     include 'footer.php';      -->
+    <!-- // ?> -->
 </body>
 </html>
--- serve/item.php
+++ serve/item.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./static/js/index.js" defer></script>
-    <link rel="stylesheet" href="/static/css/index.css">
     <title>Document</title>
 </head>
 <body>
-    <?php
-        include navbar.php;
+    <?php 
+    include './serve/navbar.php'
+    ?>
+    
+    content
 
-       
-        include footer.php     
+    <?php
+    include './serve/footer.php'
     ?>
 </body>
 </html>
