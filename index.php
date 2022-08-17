a5c41bf66af4d0ca8816792fe262102a370b6aa6
jay ber <jayber1@gmail.com>
2022-08-17 09:19:15 +0300

    add

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Script tags to external services -->
+    <script src="https://kit.fontawesome.com/4d1964c718.js" crossorigin="anonymous"></script>
     <script src="./static/js/index.js" defer></script>
+    <!-- Stylesheets links internal and external -->
     <link rel="stylesheet" href="/static/css/index.css">
     <title>Document</title>
 </head>
@@ -13,9 +16,9 @@
     <?php
         include './serve/navbar.php';
     ?>
+
     <a href="lala.php"> yabalulu-yabadabadu</a>
     
-
     <?php
         include './serve/footer.php'
     ?>
