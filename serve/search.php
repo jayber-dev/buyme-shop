b4b66bebb9457e5ae3916ef06ef9ef3a18f9020b
jayber <jayber1@gmail.com>
2022-08-19 18:22:53 +0300

    search bar added and some styling done

--- serve/search.php
+++ serve/search.php
@@ -1,7 +1,7 @@
 
 <div class="search-bar-container">
-    <form action="" method="get" class="form-display">
-        <input type="text" class="search-text-input">
+    <form action="" method="get" class="search-form-display">
+        <input type="text" class="search-text-input" placeholder="Search item here...">
         <button class="search-button">    
             <svg class="search-button-svg">
                 <image href="../static/svg/magnifying-glass-svgrepo-com.svg" class="search-button-svg"></image>
