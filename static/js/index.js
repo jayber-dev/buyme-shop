f6ef2c87a137430de9050cd5bdc81e4ffd477e42
RoniKkuky <107131096+RoniKkuky@users.noreply.github.com>
2022-08-20 12:02:47 +0300

    hide menu on scroll

--- static/js/index.js
+++ static/js/index.js
@@ -4,20 +4,31 @@ const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 
 // Toggle mobile menu 
-function toggleMenu(){
-   if(menu.classList.contains("active")){
-    menu.classList.remove("active");
-    toggle.querySelector("a").innerHTML = '<i class="fas fa-bars"></i>';
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = '<i class="fas fa-bars"></i>';
 
-// adds the menu (hamburger) icon
-   }else{
-    menu.classList.add("active");
+        // adds the menu (hamburger) icon
+    } else {
+        menu.classList.add("active");
 
-// adds the close (x) icon
-    toggle.querySelector("a").innerHTML = '<i class="fas fa-times"></i>';
-   }
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = '<i class="fas fa-times"></i>';
+    }
 }
 // Event Listener 
 toggle.addEventListener('click', toggleMenu, false);
 
 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
