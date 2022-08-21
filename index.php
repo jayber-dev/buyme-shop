defd8473b37cfa3dd73e35009229193fca3c7d30
krollkim <kimkroll2000@gmail.com>
2022-08-21 18:00:04 +0300

    commiting for ivgenny

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     <link rel="stylesheet" href="/static/css/navbar.css">
     <link rel="stylesheet" href="/static/css/footer.css">
     <link rel="stylesheet" href="/static/css/search-bar.css">
+    <link rel="stylesheet" href="/static/css/signup-form.css">
+    
     <title>Buyme</title>
 </head>
 <body>
--- serve/navbar.php
+++ serve/navbar.php
@@ -3,7 +3,7 @@
 
     <ul class="menu">
             <li class="logo"><a href="#">online shop</a></li>
-            <li class="item"><a href="#">Home</a></li>
+            <li class="item"><a href="index.php">Home</a></li>
             <li class="item"><a href="#">Electronic</a></li>
             <li class="item"><a href="#">Clothing</a></li>
             <li class="item"><a href="#">Kitchen</a></li>
@@ -11,7 +11,7 @@
             <li class="item"><a href="#">Vehicles</a></li>
             
             <li class="item button"><a href="#">Log In</a></li>
-            <li class="item button secondary"><a href="#">Sign Up</a></li>
+            <li class="item button secondary"><a href="signup-form">Sign Up</a></li>
             <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
     </ul>
 
